a9d3cb0526d27b3cbcf65fd71ce5af320354f572
Amlkassem <94128918+Amlkassem@users.noreply.github.com>
2023-03-16 20:58:24 +0200

    Add files via upload

--- SJF/src/SJF/SJF.java
+++ SJF/src/SJF/SJF.java
@@ -7,12 +7,12 @@ public class SJF {
 	System.out.println ("enter no of process:");
 	int n = sc.nextInt();
 	int pid[] = new int[n];
-	int at[] = new int[n]; 
-	int bt[] = new int[n];
-	int ct[] = new int[n]; 
-	int ta[] = new int[n]; 
-	int wt[] = new int[n];  
-	int f[] = new int[n];  
+	int at[] = new int[n]; // at means arrival time
+	int bt[] = new int[n]; // bt means burst time
+	int ct[] = new int[n]; // ct means complete time
+	int ta[] = new int[n]; // ta means turn around time
+	int wt[] = new int[n];  //wt means waiting time
+	int f[] = new int[n];  // f means it is flag it checks process is completed or not
 	int st=0, tot=0;
 	float avgwt=0, avgta=0;
 	 
@@ -20,8 +20,12 @@ public class SJF {
 	{
 	System.out.println ("enter process " + (i+1) + " arrival time:");
 	at[i] = sc.nextInt();
+	}
+	for(int i=0;i<n;i++)
+	{
 	System.out.println ("enter process " + (i+1) + " brust time:");
 	bt[i] = sc.nextInt();
+
 	pid[i] = i+1;
 	f[i] = 0;
 	}
@@ -29,16 +33,21 @@ public class SJF {
 	while(true)
 	{
 	int c=n, min=999;
-	if (tot == n) 
+	if (tot == n) // total no of process = completed process loop will be terminated
 	break;
 	for (int i=0; i<n; i++)
 	{
+	/*
+	* If i'th process arrival time <= system time and its flag=0 and burst<min
+	* That process will be executed first
+	*/
 	if ((at[i] <= st) && (f[i] == 0) && (bt[i]<min))
 	{
 	min=bt[i];
 	c=i;
 	}
 	}
+	/* If c==n means c value can not updated because no process arrival time< system time so we increase the system time */
 	if (c==n)
 	st++;
 	else
@@ -58,8 +67,8 @@ public class SJF {
 	avgta+= ta[i];
 	System.out.println(pid[i]+"\t"+at[i]+"\t"+bt[i]+"\t"+ct[i]+"\t"+ta[i]+"\t"+wt[i]);
 	}
-	System.out.println ("average waiting time  = "+ (float)(avgwt/n));
-	System.out.println ("\naverage turnaround time = "+ (float)(avgta/n));
+	System.out.println ("average waiting time is "+ (float)(avgwt/n));
+	System.out.println ("\naverage turnarund time is "+ (float)(avgta/n));
 	sc.close();
 	}
 }
